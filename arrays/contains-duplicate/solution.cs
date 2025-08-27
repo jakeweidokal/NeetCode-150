81dd352b23c1041e0ccc2a1a9e2638791a36d6aa
Jacob Weidokal <35669473+jakeweidokal@users.noreply.github.com>
2025-08-27 12:56:21 -0400

    Update solution.cs

--- arrays/contains-duplicate/solution.cs
+++ arrays/contains-duplicate/solution.cs
@@ -1,1 +1,16 @@
+var sol = new Solution();
+Console.WriteLine(sol.hasDuplicate([1, 2, 2, 3]));
 
+public class Solution
+{
+    public bool hasDuplicate(int[] nums)
+    {
+        var hashset = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (hashset.Contains(nums[i])) return true;
+            hashset.Add(nums[i]);
+        }
+        return false;
+    }
+}
