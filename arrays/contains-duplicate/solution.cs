8c4ae8016a4567a96b95ef2b3c4e75b5473930af
Jacob Weidokal <35669473+jakeweidokal@users.noreply.github.com>
2025-08-27 13:05:46 -0400

    Update solution.cs

--- arrays/contains-duplicate/solution.cs
+++ arrays/contains-duplicate/solution.cs
@@ -14,3 +14,9 @@ public class Solution
         return false;
     }
 }
+
+/* Using a HashSet has O(n) time and space complexity.
+HashSet is the primary data structure that is useful here.
+For an even simpler solution, you could drop the whole array into a HashSet. This will automatically remove any duplicates, and then you could compare the length of the hash set with the length of the original array.
+
+For better space complexity, but worse time complexity, you could sort the array, and then in each iteration compare the current value to the next value to see if it is a duplicate. */
